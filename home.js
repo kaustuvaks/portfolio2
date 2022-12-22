72444339084d794e91697aacd3b07ecce6bea4a8
kaustuvaks <kaustuvaks@gmail.com>
2022-12-22 14:36:23 +0530

    sections added

--- home.js
+++ home.js
@@ -1,6 +1,25 @@
-function openNav() {
+// function check(){
+//     ch = false;
+// }
+let hamburgerClicked = false;
+const button = document.getElementById('hamburger');
+button.addEventListener("click", function openNav(){
     document.getElementById("mySidenav").style.width = "250px";
-}
+
+    if(hamburgerClicked){
+        closeNav();
+    }
+    hamburgerClicked=!hamburgerClicked;
+})
+
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     check = false;
+//     check = !check;
+//     if(!check){
+//         closeNav();
+//     }
+// }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
