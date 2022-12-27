528d3400778ea29348d0887fee3d485d3508ce12
kaustuvaks <kaustuvaks@gmail.com>
2022-12-27 20:33:14 +0530

    proj added

--- home.js
+++ home.js
@@ -50,7 +50,8 @@ function hiding(){
     if(el_autohide){
         var last_scroll_top = 30;
         window.addEventListener('scroll', function() {
-                let scroll_top = window.scrollY;
+            closeNav();
+            let scroll_top = window.scrollY;
             if(scroll_top < last_scroll_top) {
                     el_autohide.classList.remove('scrolled-down');
                     el_autohide.classList.add('scrolled-up');
@@ -66,3 +67,41 @@ function hiding(){
     // if
   
 }
+
+// $(document).ready(function() {
+
+//     //window and animation items
+//     var animation_elements = $.find('.animation-element');
+//     var web_window = $(window);
+  
+//     //check to see if any animation containers are currently in view
+//     function check_if_in_view() {
+//       //get current window information
+//         var window_height = web_window.height();
+//         var window_top_position = web_window.scrollTop();
+//         var window_bottom_position = (window_top_position + window_height);
+  
+//       //iterate through elements to see if its in view
+//         $.each(animation_elements, function() {
+    
+//             //get the element sinformation
+//             var element = $(this);
+//             var element_height = $(element).outerHeight();
+//             var element_top_position = $(element).offset().top;
+//             var element_bottom_position = (element_top_position + element_height);
+    
+//             //check to see if this current container is visible (its viewable if it exists between the viewable space of the viewport)
+//             if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+//             element.addClass('in-view');
+//             } else {
+//             element.removeClass('in-view');
+//             }
+//         });
+  
+//     }
+//     $(window).on('scroll resize', function() {
+//         check_if_in_view()
+//     })
+//     $(window).trigger('scroll');
+  
+// });
