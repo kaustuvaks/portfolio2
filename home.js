c2e12d745e4ef27e71b306aca260e511da68086d
kaustuvaks <kaustuvaks@gmail.com>
2022-12-27 13:47:19 +0530

    js fixes

--- home.js
+++ home.js
@@ -1,27 +1,73 @@
+var prevScrollpos = window.pageYOffset;
+navbar_height = document.querySelector('.navbar').offsetHeight;
 // function check(){
 //     ch = false;
 // }
-let maxWidth = screen.width;
+
 let hamburgerClicked = false;
+
 const button = document.getElementById('hamburger');
 button.addEventListener("click", function openNav(){
-    document.getElementById("mySidenav").style.width = "250px";
+    let maxWidth = screen.width;
+    if(maxWidth<501){
+        document.getElementById("mySidenav").style.width = maxWidth+"px";
+    }else{
+        document.getElementById("mySidenav").style.width = "250px";
 
-    if(hamburgerClicked){
+    } 
+    if(!hamburgerClicked){
         closeNav();
     }
     hamburgerClicked=!hamburgerClicked;
-})
+});
 
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "250px";
-//     check = false;
-//     check = !check;
-//     if(!check){
-//         closeNav();
-//     }
-// }
+button.addEventListener("click", function(){
+    el_autohide = document.querySelector(".autohide");
+    el_autohide.classList.add('scrolled-down');
+});
+
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0px";
 }
+
+
+document.addEventListener("DOMContentLoaded", hiding());
+
+function hiding(){
+
+    el_autohide = document.querySelector(".autohide");
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    // document.body.style.paddingTop = navbar_height + 'px';
+  
+    if(el_autohide){
+        var last_scroll_top = 30;
+        window.addEventListener('scroll', function() {
+                let scroll_top = window.scrollY;
+            if(scroll_top < last_scroll_top) {
+                    el_autohide.classList.remove('scrolled-down');
+                    el_autohide.classList.add('scrolled-up');
+                }
+                else {
+                    el_autohide.classList.remove('scrolled-up');
+                    el_autohide.classList.add('scrolled-down');
+                }
+                last_scroll_top = scroll_top;
+        }); 
+      // window.addEventListener
+    }
+    // if
+  
+}
+
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+  
+//     // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+//     if (currentScrollPos > 0) {
+//       // I am using 'display' instead of 'top':
+//       document.getElementById("nav-scroll").style.display = "none";
+//     } else {
+//       document.getElementById("nav-scroll").style.display = "initial";
+//     }
+// }
