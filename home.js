27eaa511ff3b4ccac205a31a574bcbfd6178049f
kaustuvaks <kaustuvaks@gmail.com>
2022-12-28 15:25:40 +0530

    refactors

--- home.js
+++ home.js
@@ -54,3 +54,9 @@ function hiding(){
 }
 
 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
